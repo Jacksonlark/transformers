7280cbe3242d0c9c98f04b4037e358bd64766869
Jacksonlark <jacksonlark@gmail.com>
2023-04-03 18:07:53 +0800

    delete log user cache

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -579,9 +579,6 @@ class LlamaModel(LlamaPreTrainedModel):
 
         if self.gradient_checkpointing and self.training:
             if use_cache:
-                logger.warning(
-                    "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
-                )
                 use_cache = False
 
         # decoder layers
